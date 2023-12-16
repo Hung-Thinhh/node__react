997e1867920b9cc9326d474eec6529ff94213f49
Hung-Thinhh <hungthinhh2003@gmail.com>
2023-12-17 00:14:41 +0700

    modified:   src/migrations/20231212091430-create-user.js 	new file:   src/migrations/migration-group-role.js 	new file:   src/migrations/migration-group.js 	new file:   src/migrations/migration-project-user.js 	new file:   src/migrations/migration-project.js 	new file:   src/migrations/migration-role.js 	new file:   src/models/group.js 	new file:   src/models/groupRole.js 	new file:   src/models/project.js 	new file:   src/models/projectUser.js 	new file:   src/models/role.js 	modified:   src/models/user.js

--- src/models/role.js
+++ src/models/role.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Role extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,23 +11,18 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.belongs(models.Group)
-      User.belongsToMany(models.Project, {through: 'Project_User'})
+      Role.belongsToMany(models.Group, {through: 'Group_Role'})
 
     }
   }
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    username: DataTypes.STRING,
-    address: DataTypes.STRING,
-    sex: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    groupId: DataTypes.INTEGER,
+  Role.init({
+    url: DataTypes.STRING,
+    description: DataTypes.STRING,
+    
 
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Role',
   });
-  return User;
+  return Role;
 };
